af69d9941da0db086d03bf363cac3ad4abb5e51b
Ismail Ismailov <ismail2ov@gmail.com>
2023-03-20 01:16:50 +0100

    The rover can turn left

---
+++ src/test/java/com/paradigmadigital/RoverRotateLeftTest.java
@@ -0,0 +1,32 @@
+package com.paradigmadigital;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class RoverRotateLeftTest {
+
+  public static Stream<Arguments> provideDataForRotateLeft() {
+    return Stream.of(
+        Arguments.of(Orientation.NORTH, Orientation.WEST),
+        Arguments.of(Orientation.WEST, Orientation.SOUTH),
+        Arguments.of(Orientation.SOUTH, Orientation.EAST),
+        Arguments.of(Orientation.EAST, Orientation.NORTH)
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideDataForRotateLeft")
+  void testRotateLeft(Orientation initial, Orientation expected) {
+    Plateau plateau = new Plateau(5, 5);
+    Coordinate coordinate = Coordinate.of(0, 0);
+    Rover rover = new Rover(plateau, coordinate, initial);
+
+    rover.rotateLeft();
+
+    assertThat(rover.getOrientation()).isEqualTo(expected);
+  }
+}
